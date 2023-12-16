5e84d28df780cb40f212769d85f2adfea888cbb0
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-12-16 23:15:01 +0100

    refactor code to pass leetcode tests

--- src/334-string-compression.ts
+++ src/334-string-compression.ts
@@ -1,45 +1,25 @@
-export function compress(chars: string[]): string[] {
-  // chars = ["a","a","b","b","c","c","c"]
-  // result = ["a","2","b","2","c","3"]
+export function compress(chars: string[]): number {
+  let index = 0;
+  let i = 0;
 
-  // chars = ["a"]
-  // result = ["a"]
+  while (i < chars.length) {
+    let j = i;
 
-  // chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
-  // result = ["a","b","1","2"]
-
-  // For clarification, you need change the chars array,
-  // then return the length of the "new" char[].
-  // The testcase will then look at the first x elements of
-  // the chars array, where x denotes the return value.
-
-  const result: string[] = [];
-  let count = 1;
-
-  for (let i = 0; i < chars.length; i++) {
-    let currentChar = chars[i];
-    let prevChar = chars[i - 1];
-    let nextChar = chars[i + 1];
-
-    if (currentChar === prevChar) {
-      count++;
-    } else {
-      count = 1;
+    while (j < chars.length && chars[j] === chars[i]) {
+      j++;
     }
 
-    if (currentChar !== nextChar) {
-      if (count > 10) {
-        result.push(currentChar);
-        let bigCount = count.toString().split("");
-        result.push(bigCount[0], bigCount[1]);
-      } else if (count > 1) {
-        result.push(currentChar);
-        result.push(count.toString());
-      } else {
-        result.push(currentChar);
+    chars[index++] = chars[i];
+    if (j - i > 1) {
+      let count = (j - i).toString();
+
+      for (let c of count) {
+        chars[index++] = c;
       }
     }
+
+    i = j;
   }
 
-  return result;
+  return index;
 }
