94d9e54f39c3531c393e832edab7e8da970869cd
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-12-25 17:58:07 +0100

    Add time and space complexity comments to reverseList function

--- src/206-reverseList.ts
+++ src/206-reverseList.ts
@@ -3,6 +3,9 @@ import { ListNode } from "./class/NodeList";
 // Thank you madiweaver for this solution
 // link, https://leetcode.com/madiweaver/
 
+// time complexity of the reverseList function is O(n).
+// The space complexity of the function is O(n).
+
 export function reverseList(head: ListNode | null): ListNode | null {
   if (!head) return head;
 
