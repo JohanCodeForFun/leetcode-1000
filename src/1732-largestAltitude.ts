28257c157e585e248d7c12b2199d5a5e26809632
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-12-26 14:53:21 +0100

    add time and space complexity

--- src/1732-largestAltitude.ts
+++ src/1732-largestAltitude.ts
@@ -1,9 +1,20 @@
+/*
+  Complexity Analysis
+    Time complexity: O(N).
+    Space complexity: O(1).
+
+  We iterate over every integer in the list gain only once, 
+  and hence the total time complexity is equal to O(N).
+
+  We only need two variables, currentAltitude andhighestPoint; 
+  hence the space complexity is constant.
+*/
+
 export function largestAltitude(gain: number[]): number {
   let altitude = 0;
   let ans = 0;
 
   for (let i = 0; i < gain.length; i++) {
-    console.log(altitude, gain[i]);
     altitude += gain[i];
 
     ans = Math.max(ans, altitude);
