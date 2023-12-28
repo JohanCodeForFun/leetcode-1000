99e17e4f176c4a714fb7a886ed0d273f1f227358
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-12-28 22:13:51 +0100

    Refactor findDifference function to use forEach and improve code readability

--- src/2215-findDifference.ts
+++ src/2215-findDifference.ts
@@ -1,21 +1,20 @@
 export function findDifference(nums1: number[], nums2: number[]): number[][] {
-
   const set1 = new Set(nums1);
   const set2 = new Set(nums2);
 
-  const result1 = [];
-  const result2 = [];
+  const answer: number[][] = [[], []];
 
-  for (let i = 0; i < nums1.length; i++) {
-    if (set1.has(set2[i])) {
-      //result1.push(set2[i])
+  set1.forEach((value) => {
+    if (set2.has(value) === false) {
+      answer[0].push(value);
     }
-      // console.log("hey", set1.has(2))
-      // console.log("hoo", set2.has(2))
-    if (set2.has(set1[i])) {
-      //result2.push(set1[i])
-    }
-  }
+  });
 
-  return [[1], [2]];
-};
+  set2.forEach((value) => {
+    if (set1.has(value) === false) {
+      answer[1].push(value);
+    }
+  });
+
+  return answer;
+}
