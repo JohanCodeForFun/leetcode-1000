065053164bbb38005228d6eeb8b43a31194e3cde
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-12-31 22:31:24 +0100

    Refactor closeStrings function to improve time complexity

--- src/1657-closeStrings.ts
+++ src/1657-closeStrings.ts
@@ -1,9 +1,11 @@
 /*
-  Time Complexity: O(n log n)
+  Time Complexity: O(n)
   Space Complexity: O(1)
 
   Thank you alihaideralsawad for this solution!
   Link, https://leetcode.com/alihaideralsawad/
+
+  Thank you github copilot for this improved solution!
 */
 
 export function closeStrings(word1: string, word2: string): boolean {
@@ -15,10 +17,13 @@ export function closeStrings(word1: string, word2: string): boolean {
   for (const c of word1) w1c[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
   for (const c of word2) w2c[c.charCodeAt(0) - 'a'.charCodeAt(0)]++;
 
-  if (!w1c.every((val, i) => (val !== 0) === (w2c[i] !== 0)))
+  if (!w1c.every((val, i) => (val !== 0) === (w2c[i] !== 0))) return false;
 
-  w1c.sort();
-  w2c.sort();
+  const freq1 = new Array(word1.length + 1).fill(0);
+  const freq2 = new Array(word2.length + 1).fill(0);
 
-  return w1c.every((val, i) => val === w2c[i])
+  for (const c of w1c) freq1[c]++;
+  for (const c of w2c) freq2[c]++;
+
+  return freq1.every((val, i) => val === freq2[i]);
 };
