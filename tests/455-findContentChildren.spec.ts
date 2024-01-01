a325c3bfd8b98d1c65f3da20103e7bad3a3490a5
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-01 20:40:52 +0100

    add edge case in 455-findContentChildren.spec.ts

--- tests/455-findContentChildren.spec.ts
+++ tests/455-findContentChildren.spec.ts
@@ -11,22 +11,6 @@
   Your goal is to maximize the number of your 
   content children and output the maximum number.
 
- 
-
-Example 1:
-Input: g = [1,2,3], s = [1,1]
-Output: 1
-Explanation: You have 3 children and 2 cookies. The greed factors of 3 children are 1, 2, 3. 
-And even though you have 2 cookies, since their size is both 1, you could only make the child whose greed factor is 1 content.
-You need to output 1.
-
-Example 2:
-Input: g = [1,2], s = [1,2,3]
-Output: 2
-Explanation: You have 2 children and 3 cookies. The greed factors of 2 children are 1, 2. 
-You have 3 cookies and their sizes are big enough to gratify all of the children, 
-You need to output 2.
- 
 Constraints:
   1 <= g.length <= 3 * 104
   0 <= s.length <= 3 * 104
@@ -35,7 +19,7 @@ Constraints:
 
 import { findContentChildren } from "../src/455-findContentChildren";
 
-describe("test can place flowers. 605", () => {
+describe("455: Find Content Children", () => {
 
   it("first test", () => {
     const g = [1,2,3], s = [1,1]
@@ -48,6 +32,15 @@ describe("test can place flowers. 605", () => {
   
   it("second test", () => {
     const g = [1,2], s = [1,2,3]
+    const result = 2;
+    
+    const test = findContentChildren(g, s);
+
+    expect(test).toBe(result);
+  });
+
+  it("edge case", () => {
+    const g = [10,9,8,7], s = [5,6,7,8];
     const result = 2;
     
     const test = findContentChildren(g, s);
