4122ae0ae7c0e3114de9629ec247ae4777a8d5cb
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-05 21:10:39 +0100

    Refactor predictPartyVictory function

--- src/649-predictPartyVictory.ts
+++ src/649-predictPartyVictory.ts
@@ -1,21 +1,34 @@
 /*
   Time Complexity: O(n)
   Space Complexity: O(n)
+
+  Thank you Santosh Setty for getting my to the solution!
+  Link, https://leetcode.com/problems/dota2-senate/discuss/3483505/Step-by-Step-Guide%3A-JavaScript-Code-with-Comments-and-Detailed-Explanation
 */
 
 export function predictPartyVictory(senate: string): string {
-  let queue = senate.split("");
-  let radiantCount = 0;
-  let direCount = 0;
+  const dire = [];
+  const radiant = [];
+  const n = senate.length;
 
-  while (queue.length > 0) {
-    queue[0] === "R" ? radiantCount++ : direCount++;
-    queue.shift();
+  for (let i = 0; i < n; i++) {
+    if (senate[i] === "D") {
+      dire.push(i + n);
+    } else {
+      radiant.push(i + n);
+    }
   }
 
-  if (queue.length === 2) {
-    return queue[0] === "D" ? "Dire" : "Radiant";
+  while (dire.length > 0 && radiant.length > 0) {
+    if (dire[0] > radiant[0]) {
+      radiant.push(radiant[0] + n)
+    } else {
+      dire.push(dire[0] + n)
+    }
+
+    dire.shift();
+    radiant.shift();
   }
 
-  return radiantCount > direCount ? "Radiant" : "Dire";
+  return (radiant.length > 0) ? "Radiant" : "Dire";
 }
