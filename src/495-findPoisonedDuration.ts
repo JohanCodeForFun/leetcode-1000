fe04899fad2929ce2a96488d29d20b463f1bf9a8
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-05 22:29:42 +0100

    Refactor findPoisonedDuration function to calculate total duration more efficiently

--- src/495-findPoisonedDuration.ts
+++ src/495-findPoisonedDuration.ts
@@ -1,32 +1,22 @@
 /*
-  Input: timeSeries = [1,4], duration = 2
-  Output: 4
-  Explanation: Teemo's attacks on Ashe go as follows:
-  - At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
-  - At second 4, Teemo attacks, and Ashe is poisoned for seconds 4 and 5.
-  Ashe is poisoned for seconds 1, 2, 4, and 5, which is 4 seconds in total.
+  Space Complexity: O(n)
+  Time Complexity: O(1)
 
-  Example 2:
-  Input: timeSeries = [1,2], duration = 2
-  */
+  Difficulty, Easy
+  Tags: array, simulation 
+*/
 
 export function findPoisonedDuration(
   timeSeries: number[],
   duration: number
 ): number {
+  if (timeSeries.length === 0) return 0;
 
-  let durationCount = 0;
-  let n = timeSeries.length;
+  let totalDuration = 0;
 
-  for (let i = 0; i < n; i++) {
-    // if 1, 2 are in duration, restart counter from 2.
-    // duration 2 seconds. 1 + 2 === timeseries[i] + duration 
-    if (timeSeries[i] + timeSeries[i + 1] === timeSeries[i] + duration) {
-      durationCount += 1;
-    } else {
-      durationCount += 1 * duration;
-    }
+  for (let i = 0; i < timeSeries.length - 1; i++) {
+    totalDuration += Math.min(timeSeries[i + 1] - timeSeries[i], duration);
   }
 
-  return durationCount;
+  return totalDuration + duration;
 }
