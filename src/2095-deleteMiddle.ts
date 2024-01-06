e5c6cff570d6a3ed27269c1725a394ab33ca0edf
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-06 21:32:31 +0100

    Add deleteMiddle function and corresponding test cases

---
+++ src/2095-deleteMiddle.ts
@@ -0,0 +1,26 @@
+import { ListNode } from "./class/NodeList";
+
+/*
+  Time Complexity: O(n)
+  Space Complexity: O(1)
+*/
+
+export function deleteMiddle(head: ListNode | null): ListNode | null {
+  if (!head || !head.next) return null;
+
+  let slow = head;
+  let fast = head.next;
+
+  while (fast.next && fast.next.next) {
+    if (slow.next) {
+      slow = slow.next;
+    }
+    fast = fast.next.next;
+  }
+
+  if (slow.next) {
+    slow.next = slow.next?.next;
+  }
+
+  return head;
+}
