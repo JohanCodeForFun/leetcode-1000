a80a46dd45503971d411aa053be258a04df2be96
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-15 19:48:28 +0100

    Add solution for finding a corresponding node of a binary tree in a clone

--- src/1379-getTargetCopy.ts
+++ src/1379-getTargetCopy.ts
@@ -24,3 +24,37 @@ function getTargetCopy(
     getTargetCopy(original.right, cloned.right, target)
   );
 }
+
+/*
+Thank you Control The Narrative for this solution!
+Link, https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/discuss/998841/JavaScript-Clean-DFS-and-BFS-Solutions
+
+Solution-1: DFS
+var getTargetCopy = function(original, cloned, target) {
+  
+  function traverse(oNode, cNode) {
+      if(!oNode) return;
+      if(oNode === target) return cNode;
+      return traverse(oNode.left, cNode.left) || traverse(oNode.right, cNode.right);
+  }
+  return traverse(original, cloned);
+};
+
+
+Solution-2: BFS
+var getTargetCopy = function(original, cloned, target) {
+  let queue = [[original, cloned]];
+  
+  while(queue.length) {
+      const next = [];
+      
+      for(let [oNode, cNode] of queue) {
+          if(oNode === target) return cNode;
+          if(oNode.left) next.push([oNode.left, cNode.left]);
+          if(oNode.right) next.push([oNode.right, cNode.right]);
+      }
+      queue = next;
+  }
+};
+
+*/
