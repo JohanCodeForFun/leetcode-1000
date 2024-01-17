018230e9d0fbac949a3533189cc164050ae2b9a1
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-17 21:15:41 +0100

    Add BFS implementation for searchBST function

--- src/700-searchBST.ts
+++ src/700-searchBST.ts
@@ -5,7 +5,7 @@ import { TreeNode } from "./class/TreeNode";
   Space Complexity: O(h)
 
   Difficulty: Easy.
-  Tags: Binary Tree, DFS.
+  Tags: Binary Tree, DFS, BFS.
 */
 
 function searchBST(root: TreeNode | null, val: number): TreeNode | null {
@@ -25,3 +25,19 @@ function searchBST(root: TreeNode | null, val: number): TreeNode | null {
 
   return dfs(root, val);
 }
+
+/*
+Thank you Teabag Coder for this solution!
+Link, https://leetcode.com/problems/search-in-a-binary-search-tree/discuss/3274313/oror-BFS-oror-TYPESCRIPT-oror-69.96-oror-CLICK-oror
+
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+  let q = [root];
+  while(q.length > 0) {
+      let node = q.shift();
+      if(node.val === val) return node;
+      if(node.left) q.push(node.left);
+      if(node.right) q.push(node.right);
+  }
+  return null;
+};
+*/
