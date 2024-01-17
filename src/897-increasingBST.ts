f73810c4e9af025786ec32efa033b82d014f6a6a
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-17 21:12:19 +0100

    Add implementation of increasingBST function

---
+++ src/897-increasingBST.ts
@@ -0,0 +1,13 @@
+import { TreeNode } from "./class/TreeNode";
+
+/*
+  Time Complexity: O(n)
+  Space Complexity: O(h)
+
+  Difficulty: Easy.
+  Tags: Binary Tree, DFS.
+*/
+
+function increasingBST(root: TreeNode | null): TreeNode | null {
+    return null;
+};
