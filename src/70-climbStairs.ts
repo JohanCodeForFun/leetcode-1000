06119b53192627a5b1b73d2e7a5750158b6fe9b3
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-18 11:03:19 +0100

    WIP climbStairs function and test cases

---
+++ src/70-climbStairs.ts
@@ -0,0 +1,24 @@
+function climbStairs(n: number): number {
+  return n
+};
+/*
+case 1:
+  Input 2
+  Output 2
+  Expected 2
+
+case 2:
+  Input 3
+  Output 3
+  Expected 3
+
+case 3:
+  Input 4
+  Output 5
+  Expected 5
+
+case 5:
+  Input 4
+  Output 4
+  Expected 5
+*/
