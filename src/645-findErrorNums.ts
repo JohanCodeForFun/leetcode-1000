d0f9a0f3430034d28f4cb9de6d8e33921cd837ec
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-22 23:00:56 +0100

    Refactor findErrorNums function to work correctly

--- src/645-findErrorNums.ts
+++ src/645-findErrorNums.ts
@@ -1,46 +1,28 @@
 /*
-Example 1:
+  Time Complexity: O(n)
+  Space Complexity: O(n)
 
-Input: nums = [1,2,2,4]
-Output: [2,3]
-Example 2:
-
-Input: nums = [1,1]
-Output: [1,2]
- 
-
-Constraints:
-
-2 <= nums.length <= 104
-1 <= nums[i] <= 104
+  Diffculty, Easy
+  Tags: hash map, array. numbers
 */
-
 function findErrorNums(nums: number[]): number[] {
   let list = new Map();
-  let errorValue;
-  const result = [];
+  let duplicate = -1;
+  let missing = -1;
 
   for (let num of nums) {
     if (list.has(num)) {
-      list.set(num, list.get(num) + 1);
-    } else {
-      list.set(num, 1);
+      duplicate = num;
+    }
+    list.set(num, true);
+  }
+
+  for (let i = 1; i <= nums.length; i++) {
+    if (!list.has(i)) {
+      missing = i;
+      break;
     }
   }
 
-  for (let [key, value] of list) {
-    if (value > 1) errorValue = value;
-  }
-
-  let indexOfErrorValue = nums.indexOf(errorValue);
-
-  console.log("Error value:", errorValue, indexOfErrorValue);
-
-  for (let i = indexOfErrorValue; i < nums.length; i++) {
-    console.log(i, nums[i]);
-    result.push(nums[i]);
-    if (result[i] === nums[i - 1]) result[i] += 1;
-  }
-
-  return result;
+  return [duplicate, missing];
 }
