46857546720915a533819dc628aa9260e39ecbf7
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-01-29 20:11:59 +0100

    Refactor MyQueue class

--- src/232-MyQueue.ts
+++ src/232-MyQueue.ts
@@ -9,32 +9,32 @@
 class MyQueue {
   queue: number[];
   constructor() {
-      this.queue = [];
+    this.queue = [];
   }
 
   push(x: number): void {
-      this.queue.push(x)
+    this.queue.push(x);
   }
 
   pop(): number {
     if (this.queue.length === 0) return -1;
-      return this.queue.shift() as number;
+    return this.queue.shift() as number;
   }
 
   peek(): number {
-      return this.queue[0]
+    return this.queue[0];
   }
 
   empty(): boolean {
-      return this.queue.length > 0 ? false : true;
+    return this.queue.length > 0 ? false : true;
   }
 }
 
 /**
-* Your MyQueue object will be instantiated and called as such:
-* var obj = new MyQueue()
-* obj.push(x)
-* var param_2 = obj.pop()
-* var param_3 = obj.peek()
-* var param_4 = obj.empty()
-*/
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
