b474ae19da3cdcfaf6bf89bb43b858b248dfadd5
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-02-01 20:06:19 +0100

    Add SQL query to replace Employee ID with Unique Identifier

---
+++ src/1378-ReplaceEmployeeIDWithTheUniqueIdentifier.sql
@@ -0,0 +1,44 @@
+-- 1378. 
+-- Replace Employee ID With The Unique Identifier
+-- Easy
+-- # Write your MySQL query statement below
+SELECT Employees.name, EmployeeUNI.unique_id
+FROM Employees
+LEFT OUTER JOIN EmployeeUNI
+ON EmployeeUNI.id=Employees.id
+
+
+-- Input: 
+-- Employees table:
+-- +----+----------+
+-- | id | name     |
+-- +----+----------+
+-- | 1  | Alice    |
+-- | 7  | Bob      |
+-- | 11 | Meir     |
+-- | 90 | Winston  |
+-- | 3  | Jonathan |
+-- +----+----------+
+-- EmployeeUNI table:
+-- +----+-----------+
+-- | id | unique_id |
+-- +----+-----------+
+-- | 3  | 1         |
+-- | 11 | 2         |
+-- | 90 | 3         |
+-- +----+-----------+
+-- Output: 
+-- +-----------+----------+
+-- | unique_id | name     |
+-- +-----------+----------+
+-- | null      | Alice    |
+-- | null      | Bob      |
+-- | 2         | Meir     |
+-- | 3         | Winston  |
+-- | 1         | Jonathan |
+-- +-----------+----------+
+-- Explanation: 
+-- Alice and Bob do not have a unique ID, We will show null instead.
+-- The unique ID of Meir is 2.
+-- The unique ID of Winston is 3.
+-- The unique ID of Jonathan is 1.
