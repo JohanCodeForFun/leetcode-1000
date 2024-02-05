bd5cbdf1c594c18fc5d39c0bdd3cb05ca9c9dbdd
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-02-05 09:45:33 +0100

    Add SQL queries for various analysis tasks

---
+++ src/1070-ProductSalesAnalysis-III.sql
@@ -0,0 +1,35 @@
+-- # Write your MySQL query statement below
+SELECT Sales.product_id, Sales.year as first_year, Sales.quantity, Sales.price 
+FROM Sales
+WHERE (product_id, year) in (
+SELECT product_id, MIN(year)
+    FROM Sales
+    GROUP BY product_id
+)
+
+--
+-- Example 1:
+-- Input: 
+-- Sales table:
+-- +---------+------------+------+----------+-------+
+-- | sale_id | product_id | year | quantity | price |
+-- +---------+------------+------+----------+-------+ 
+-- | 1       | 100        | 2008 | 10       | 5000  |
+-- | 2       | 100        | 2009 | 12       | 5000  |
+-- | 7       | 200        | 2011 | 15       | 9000  |
+-- +---------+------------+------+----------+-------+
+-- Product table:
+-- +------------+--------------+
+-- | product_id | product_name |
+-- +------------+--------------+
+-- | 100        | Nokia        |
+-- | 200        | Apple        |
+-- | 300        | Samsung      |
+-- +------------+--------------+
+-- Output: 
+-- +------------+------------+----------+-------+
+-- | product_id | first_year | quantity | price |
+-- +------------+------------+----------+-------+ 
+-- | 100        | 2008       | 10       | 5000  |
+-- | 200        | 2011       | 15       | 9000  |
+-- +------------+------------+----------+-------+
---
+++ src/1729-FindFollowersCount.sql
@@ -0,0 +1,30 @@
+# Write your MySQL query statement below
+SELECT user_id, COUNT(follower_id) as followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id ASC;
+
+--
+-- Example 1:
+-- Input: 
+-- Followers table:
+-- +---------+-------------+
+-- | user_id | follower_id |
+-- +---------+-------------+
+-- | 0       | 1           |
+-- | 1       | 0           |
+-- | 2       | 0           |
+-- | 2       | 1           |
+-- +---------+-------------+
+-- Output: 
+-- +---------+----------------+
+-- | user_id | followers_count|
+-- +---------+----------------+
+-- | 0       | 1              |
+-- | 1       | 1              |
+-- | 2       | 2              |
+-- +---------+----------------+
+-- Explanation: 
+-- The followers of 0 are {1}
+-- The followers of 1 are {0}
+-- The followers of 2 are {0,1}
