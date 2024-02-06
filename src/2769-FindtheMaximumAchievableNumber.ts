3b5664a4436784e18365ff2737ce78f7b31a7c59
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-02-06 22:34:24 +0100

    Add solution for problem 2769: Find the Maximum Achievable Number

---
+++ src/2769-FindtheMaximumAchievableNumber.ts
@@ -0,0 +1,11 @@
+/*
+  Time Complexity: O(1)
+  Space Complexity: O(1)
+
+  Difficulty: Easy
+  Tags: Math
+*/
+
+function theMaximumAchievableX(num: number, t: number): number {
+  return num + (t * 2);
+};
