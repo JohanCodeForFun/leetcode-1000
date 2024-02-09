8d4c0fe07db2676df9e5947a92a01f77111911d4
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-02-09 21:51:47 +0100

    Add one-line solution

--- src/2798-NumberofEmployeesWhoMettheTarget.ts
+++ src/2798-NumberofEmployeesWhoMettheTarget.ts
@@ -7,6 +7,8 @@
 */
 
 function numberOfEmployeesWhoMetTarget(hours: number[], target: number): number {
+  // return hours.filter((hour)=>hour>=target).length;
+
   let targetCount = 0;
 
   for (let hour of hours) {
