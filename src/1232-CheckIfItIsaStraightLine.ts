6b6d013274e17a8792b2f98841a424744f5139a0
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-02-11 21:03:46 +0100

    Refactor checkStraightLine function to improve performance and readability

--- src/1232-CheckIfItIsaStraightLine.ts
+++ src/1232-CheckIfItIsaStraightLine.ts
@@ -1,25 +1,28 @@
+/*
+  Time Complexity: O(n)
+  Space Complexity: O(1)
+
+  Difficulty: Easy
+  Tags: Array, math
+*/
+
 function checkStraightLine(coordinates: number[][]): boolean {
-  let temp = [];
-  let result = false
+  let x1 = coordinates[0][0];
+  let x2 = coordinates[1][0];
+  let y1 = coordinates[0][1];
+  let y2 = coordinates[1][1];
 
-  for (let i = 1; i < coordinates.length; i++) {
-    let x1 = coordinates[i - 1][0];
-    let x2 = coordinates[i][0];
-    let y1 = coordinates[i - 1][1];
-    let y2 = coordinates[i][1];
+  let dx = x2 - x1;
+  let dy = y2 - y1;
 
-    let x = x2 - x1;
-    let y = y2 - y1;
+  for (let i = 2; i < coordinates.length; i++) {
+    let x = coordinates[i][0];
+    let y = coordinates[i][1];
 
-    let k = y / x;
-
-    temp.push(k)
+    if (dy * (x - x2) !== dx * (y - y2)) {
+      return false;
+    }
   }
 
-  const set = new Set(temp)
-
-  console.log(temp, set)
-
-  return set.size === 1 ? true : false
-
+  return true;
 };
