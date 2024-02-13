98db85f826c99a292467262c264e8921edde99a4
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-02-13 20:31:59 +0100

    refactor firstPalindrome function to find the first palindromic string in an array

--- src/2108-FindFirstPalindromicStringintheArray.ts
+++ src/2108-FindFirstPalindromicStringintheArray.ts
@@ -1,25 +1,30 @@
+/*
+  Time Complexity: O(n)
+  Space Complexity:O(1)
+
+  Difficulty: Easy
+  Tags: String, two pointer
+*/
+
 function firstPalindrome(words: string[]): string {
   for (const word of words) {
     let n = word.length
     let left = 0;
     let right = n - 1;
 
-    if (word.length === 1) {
-      return word;
-    };
-
     while (left < right) {
 
       if (word[left] !== word[right]) {
         break;
-      } else {
-        left++;
-        right--;
       };
 
-      return word;
+      left++;
+      right--;
     };
 
+    if (left >= right) {
+      return word;
+    };
   };
 
   return "";
