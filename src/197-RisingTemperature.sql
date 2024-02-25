dac90026aa017bb57c13e70f6ea010ccabb83bcf
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-02-25 21:10:59 +0100

    Add SQL query to find rising temperature

---
+++ src/197-RisingTemperature.sql
@@ -0,0 +1,29 @@
+-- # Write your MySQL query statement below
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2
+ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
+WHERE w1.temperature > w2.temperature;
+
+--
+-- Example 1:
+-- Input: 
+-- Weather table:
+-- +----+------------+-------------+
+-- | id | recordDate | temperature |
+-- +----+------------+-------------+
+-- | 1  | 2015-01-01 | 10          |
+-- | 2  | 2015-01-02 | 25          |
+-- | 3  | 2015-01-03 | 20          |
+-- | 4  | 2015-01-04 | 30          |
+-- +----+------------+-------------+
+-- Output: 
+-- +----+
+-- | id |
+-- +----+
+-- | 2  |
+-- | 4  |
+-- +----+
+-- Explanation: 
+-- In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+-- In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
