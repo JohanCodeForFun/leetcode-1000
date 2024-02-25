e2cd4e5635a41a6e46082e56403890a1f0966c21
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-02-25 21:03:54 +0100

    Add SQL query to find classes with more than 5 students

---
+++ src/596-ClassesMoreThan5Students.sql
@@ -0,0 +1,34 @@
+-- # Write your MySQL query statement below
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(DISTINCT student) >= 5;
+
+--
+-- Example 1:
+-- Input: 
+-- Courses table:
+-- +---------+----------+
+-- | student | class    |
+-- +---------+----------+
+-- | A       | Math     |
+-- | B       | English  |
+-- | C       | Math     |
+-- | D       | Biology  |
+-- | E       | Math     |
+-- | F       | Computer |
+-- | G       | Math     |
+-- | H       | Math     |
+-- | I       | Math     |
+-- +---------+----------+
+-- Output: 
+-- +---------+
+-- | class   |
+-- +---------+
+-- | Math    |
+-- +---------+
+-- Explanation: 
+-- - Math has 6 students, so we include it.
+-- - English has 1 student, so we do not include it.
+-- - Biology has 1 student, so we do not include it.
+-- - Computer has 1 student, so we do not include it.
