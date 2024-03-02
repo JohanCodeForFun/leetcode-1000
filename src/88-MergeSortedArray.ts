c6697e03250c46cd040a1708e6d5d57abc7295d2
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-03-02 14:26:04 +0100

    Add solution for merging sorted arrays

---
+++ src/88-MergeSortedArray.ts
@@ -0,0 +1,19 @@
+/*
+  Time Complexity: O(n)
+  Space Complexity: O(1)
+
+  Difficulty: Easy
+  Tags: Array, numbers
+*/
+
+/**
+ Do not return anything, modify nums1 in-place instead.
+*/
+
+ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  for (let i = 0; i < n; i++) {
+      nums1[i + m] = nums2[i] 
+  }
+  
+  nums1.sort((a, b) => a - b)
+};
