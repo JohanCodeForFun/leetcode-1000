dbc5052c6d70177e5422e31ebf3b69b2c393571f
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-03-04 21:44:06 +0100

    Add solution for bagOfTokensScore problem

---
+++ src/948-bagOfTokensScore.ts
@@ -0,0 +1,32 @@
+/*
+  Time Complexity: O(n)
+  Space Complexity: O(1)
+
+  Difficulty: Medium
+  Tags: Array, numbers, two pointer, daily
+*/
+
+function bagOfTokensScore(tokens: number[], power: number): number {
+  let low = 0;
+  let high = tokens.length - 1;
+  let score = 0;
+
+  tokens = tokens.sort((a, b) => a - b);
+
+  while (low <= high) {
+    if (power >= tokens[low]) {
+      score++;
+      power -= tokens[low];
+      low++;
+
+    } else if (score > 0 && low < high) {
+      score--;
+      power += tokens[high];
+      high--;
+    } else {
+      return score
+    }
+  }
+
+  return score
+};
