39718b2a85478d91d4c85a2c23c6bb40f6f090d3
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-03-11 21:26:31 +0100

    Refactor customSortString function to improve efficiency

--- src/791-customSortString.ts
+++ src/791-customSortString.ts
@@ -1,44 +1,31 @@
 /*
-  Time Complexity:
-  Space Complexity:
+  Time Complexity: O(n + m)
+  Space Complexity: O(m)
 
   Difficulty: Medium
   Tags: String, hash map, daily
+
+  Thank you Deepak Negi for helping me simplify the algorithm!
+  Link, https://leetcode.com/problems/custom-sort-string/discuss/4856371/100-Beats-or-or-Hash-Table-or-or-Easy-Explanation-or-or-C%2B%2B-or-or-Java-or-or-Python-or-or
 */
 
 function customSortString(order: string, s: string): string {
-  let sMap = new Map<string, number>();
-  let orderMap = new Map<string, number>();
+  let map = new Map<string, number>();
+  let result = '';
 
   for (const char of s) {
-    sMap.set(char, (sMap.get(char) || 0) + 1)
+    map.set(char, (map.get(char) || 0) + 1);
   }
+
   for (const char of order) {
-    orderMap.set(char, (orderMap.get(char) || 0) + 1);
-  }
-
-  let orderArr = order.split('')
-  let sArr = s.split('')
-  let result = '';
-  let i = 0
-
-  console.log(orderArr, sArr)
-
-  while (orderArr.length > 0) {
-    if (orderMap.has(s[i])) {
-      result += order[i]
-      orderArr.shift();
-      sArr.shift()
-      i++;
-    } else {
-      result += order[i];
-      sArr.shift();
-      i++;
+    if (map.has(char)) {
+      result += char.repeat(map.get(char) || 0);
+      map.delete(char);
     }
   }
 
-  for (i = result.length; i < s.length; i++) {
-    result += s[i]
+  for (const [char, count] of map.entries()) {
+    result += char.repeat(count || 0)
   }
 
   return result;
