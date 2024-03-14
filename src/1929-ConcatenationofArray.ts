1d82125bbedc79ef4d7b8f24761e1ba42008d1ec
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-03-14 21:27:21 +0100

    Add solution to getConcatenation

---
+++ src/1929-ConcatenationofArray.ts
@@ -0,0 +1,11 @@
+/*
+    Time Complexity: O(n)
+    Space Complexity: O(n)
+
+    Difficulty: Easy
+    Tags: Array
+*/
+
+function getConcatenation(nums: number[]): number[] {
+    return nums.concat(nums);
+}
