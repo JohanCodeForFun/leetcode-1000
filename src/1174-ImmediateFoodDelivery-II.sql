81cc9ee2fef33d26baf4b6c7c67b6137f9c5747b
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-03-30 21:53:55 +0100

    improve solution to Immediate Food Delivery II

--- src/1174-ImmediateFoodDelivery-II.sql
+++ src/1174-ImmediateFoodDelivery-II.sql
@@ -1,19 +1,15 @@
 # Write your MySQL query statement below
 SELECT
     ROUND(
-    (SUM(CASE WHEN order_date = customer_pref_delivery_date THEN 1 ELSE 0 END)
-    / COUNT(DISTINCT customer_id)) * 100, 2
+    AVG(order_date = customer_pref_delivery_date)* 100, 2
 ) AS immediate_percentage
 FROM
     Delivery
 WHERE
     (customer_id, order_date) IN (
-    SELECT
-        customer_id, MIN(order_date) AS first_order_date
-    FROM
-        Delivery
-    GROUP BY
-        customer_id
+    SELECT customer_id, MIN(order_date) AS first_order_date
+    FROM Delivery
+    GROUP BY customer_id
 );
 
 Example 1:
