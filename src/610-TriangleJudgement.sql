3859c14fce368531c182f05bf302d61f44d70d74
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-04-09 22:08:17 +0200

    redo Triangle Judgement with case solution

--- src/610-TriangleJudgement.sql
+++ src/610-TriangleJudgement.sql
@@ -1,5 +1,14 @@
 -- # Write your MySQL query statement below
-SELECT *, IF(x+y>z and y+z>x and z+x>y, "Yes", "No") as triangle FROM Triangle;
+SELECT x, y, z,
+    CASE
+        WHEN x + y > z
+        AND y + z > x
+        AND z + x > y
+        THEN 'Yes'
+        ELSE 'No'
+    END AS triangle
+FROM Triangle;
+
 
 --
 -- Example 1:
