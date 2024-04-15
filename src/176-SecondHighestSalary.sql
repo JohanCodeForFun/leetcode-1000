cd5cc883d26c89ee1559e11054ffcf00c752d775
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-04-15 21:37:45 +0200

    Add solution to Second Highest Salary

---
+++ src/176-SecondHighestSalary.sql
@@ -0,0 +1,42 @@
+# Write your MySQL query statement below
+SELECT
+    IFNULL(
+            (SELECT DISTINCT salary
+             FROM Employee
+             ORDER BY salary DESC
+                LIMIT 1 OFFSET 1),
+        NULL
+    ) AS SecondHighestSalary;
+
+
+Example 1:
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+Output: 
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| null                |
++---------------------+
