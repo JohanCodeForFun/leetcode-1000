20fd5126fd47d1a37c4cea788045728d6a847924
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-04-22 21:27:33 +0200

    Add solution to Friend Requests II: Who Has the Most Friends

--- src/602-FriendRequests-II.sql
+++ src/602-FriendRequests-II.sql
@@ -1,10 +1,33 @@
 # Write your MySQL query statement below
-SELECT requester_id
-FROM RequestAccepted
-WHERE requester_id = accepter_id
+WITH all_ids AS (
+    SELECT requester_id AS id
+    FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id
+    FROM RequestAccepted)
+SELECT id,
+    COUNT(id) AS num
+FROM all_ids
+GROUP BY id
+ORDER BY COUNT(id) DESC
+LIMIT 1;
 
--- WHERE requester_id IN (SELECT accepter_id
---                       FROM RequestAccepted)
--- AND accepter_id IN (SELECT requester_id
---                       FROM RequestAccepted)
 
+Input:
+RequestAccepted table:
++--------------+-------------+-------------+
+| requester_id | accepter_id | accept_date |
++--------------+-------------+-------------+
+| 1            | 2           | 2016/06/03  |
+| 1            | 3           | 2016/06/08  |
+| 2            | 3           | 2016/06/08  |
+| 3            | 4           | 2016/06/09  |
++--------------+-------------+-------------+
+Output:
++----+-----+
+| id | num |
++----+-----+
+| 3  | 3   |
++----+-----+
+Explanation:
+The person with id 3 is a friend of people 1, 2, and 4, so he has three friends in total, which is the most number than any others.
